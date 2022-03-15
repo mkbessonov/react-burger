45d277ea0c4db35d44f9622d56fc6675f08563ea
mkbessonov <mkbessonov@mail.ru>
2022-03-15 22:29:08 +0300

    Для пуллреквеста

--- src/service/ingredients-service.ts
+++
@@ -1,5 +0,0 @@
-import api from "./api";
-
-export const getIngredients = async () => {
-    return await api.get(`/ingredients`);
-};
--- src/service/order-service.ts
+++
@@ -1,5 +0,0 @@
-import api from "./api";
-
-export const createOrder = async (ingredients: string[]) => {
-    return await api.post(`/orders`, {ingredients});
-};
