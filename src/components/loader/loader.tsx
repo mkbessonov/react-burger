f92c60f98b90918e495668b0a8dea0ea613a8d4a
mkbessonov <mkbessonov@mail.ru>
2022-03-31 18:49:34 +0300

    Alert and Loader

--- src/components/loader/loader.tsx
+++ src/components/loader/loader.tsx
@@ -1,16 +1,16 @@
-import './loader.css'
+import styles from './loader.module.css'
 import {createPortal} from "react-dom";
 import React from "react";
 
 const modal = document.getElementById("modal")!;
 export const Loader = () => {
-    return (createPortal(<div className='container'>
-            <div className="bars-common bar-one"/>
-            <div className="bars-common bar-two"/>
-            <div className="bars-common bar-three"/>
+    return (createPortal(<div className={styles.container}>
+            <div className={`${styles.bars_common} ${styles.bar_one}`}/>
+            <div className={`${styles.bars_common} ${styles.bar_two}`}/>
+            <div className={`${styles.bars_common} ${styles.bar_three}`}/>
 
-            <div className="squares-common square-one"/>
-            <div className="squares-common square-two"/>
+            <div className={`${styles.squares_common} ${styles.square_one}`}/>
+            <div className={`${styles.squares_common} ${styles.square_two}`}/>
         </div>, modal)
     );
 };
